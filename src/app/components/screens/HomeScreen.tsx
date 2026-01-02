8de9f5435e5f4757ccfdd207de80886e10bff6ed
DASTANSAL1EV <sal1evdastan777@gmail.com>
2026-01-03 01:54:33 +0600

    fixed second

--- src/app/components/screens/HomeScreen.tsx
+++ src/app/components/screens/HomeScreen.tsx
@@ -22,8 +22,8 @@ export function HomeScreen({ language, onLanguageToggle, onStartTransfer }: Home
 
   return (
     <ScreenLayout showLanguageSwitch currentLanguage={language} onLanguageToggle={onLanguageToggle}>
-      <div className="flex flex-col items-center justify-between min-h-[450px]">
-        <div className="flex-1 flex flex-col items-center justify-center gap-12">
+      <div className="flex flex-col items-center justify-between min-h-[370px]">
+        <div className="flex-1 flex flex-col items-center justify-center ">
             <img
                 src="/logo.png" alt="Anobanko"className="w-32 h-32 object-contain"/>
           <p className="text-center text-gray-700 leading-relaxed px-4">{content[language].description}</p>
